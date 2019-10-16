6996e55c410a52fbcbdd55756a89659bd54f8298
long-hoang <thoanglong@gmail.com>
2019-10-16 00:03:07 +0000

    new job form, authentication of users, user_id correlation with places

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,27 @@
 class JobsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :show]
 
   def index
     @jobs = Job.all
   end
 
+  def new
+    @job = Job.new
+  end
+
+  def create 
+    current_user.jobs.create(job_params)
+    redirect_to root_path
+  end
+
+  def show 
+    @job = Job.find(params[:id])
+  end
+
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :prompt, :reward)
+  end
 end
